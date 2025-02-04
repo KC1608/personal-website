5ebf71f7644aa4f0fe98850c12f6318ec4d4eb89
kartikAppscrip <114727287+kartikAppscrip@users.noreply.github.com>
2025-02-05 00:27:33 +0530

    Add Vercel Analytics to track website usage

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css"
 import { Inter } from "next/font/google"
 import Header from "./components/Header"
 import type React from "react"
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -20,6 +21,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <Header />
         {children}
+        <Analytics />
       </body>
     </html>
   )
